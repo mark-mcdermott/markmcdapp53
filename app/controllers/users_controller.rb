b72c4cc6324768bd9a29b6117e0af5da3356a6fa
Mark McDermott <mark@markmcdermott.io>
2022-04-26 03:15:53 +0000

    production settings for puma and heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
   end
 
   # GET /users/new
@@ -66,6 +67,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :admin)
+      params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
     end
 end
